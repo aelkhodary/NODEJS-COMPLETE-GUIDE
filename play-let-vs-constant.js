072aced21594f184c5914203b2c964a66fa26056
aelkhodary <40409487+aelkhodary@users.noreply.github.com>
2020-05-07 01:19:59 +0400

    init-javascript

---
+++ play-let-vs-constant.js
@@ -0,0 +1,20 @@
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+}
+
+//name ='ali';
+
+console.log(summarizeUser(name, age, hasHobbies));
