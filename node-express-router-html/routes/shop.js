fc3c6d2741abe235dff6def01da343b8da8ae460
aelkhodary <40409487+aelkhodary@users.noreply.github.com>
2020-05-21 02:16:50 +0400

    commit html mvc

---
+++ node-express-router-html/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    //__dirname current directory path
+    //res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
